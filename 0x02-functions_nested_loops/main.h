751bd8830230fba3ea07f3f8c897ef4db5ce4470
Kolinzoviola <dockolinzo@gmail.com>
2023-08-16 09:46:56 -0700

    task 5

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_h
 #define MAIN_h
 
+int print_sign(int n);
 void print_to_98(int n);
 int add(int, int);
 void times_table(void);
 void jack_bauer(void);
 int print_last_digit(int);
 int _abs(int);
-int print_sign(int n);
 int _islower(int c);
 void print_alphabet_x10(void);
 int _putchar(char c);
