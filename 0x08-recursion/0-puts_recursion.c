4d2b49690dc1c594b9f4ba16c4ca02744d00e0c6
dockolinz@gmail.com <dockolin@gmail.com>
2023-08-30 17:51:13 +0100

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _put_recursion - function like puts();
+ * _puts_recursion - function like puts();
  * @s: input
- * Return: Always 0 
+ * Return: Always 0
  */
 void _puts_recursion(char *s)
 {
