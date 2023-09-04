9ce8b49900c671d5a65dca7421d4bd040ab5f9f9
Kolinzoviola <dockolinz@gmail.com>
2023-09-04 18:07:03 +0000

    task 101 of advance

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -29,9 +29,9 @@ int count_word(char *s)
 }
 /**
  * **strtow - splits a string into words
- * @str: strings to split
+ * @str: string to split
  *
- * Return: pointer to an array of strings 
+ * Return: pointer to an array of strings (Success)
  * or NULL (Error)
  */
 char **strtow(char *str)
