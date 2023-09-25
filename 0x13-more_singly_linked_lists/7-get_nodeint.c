4c51b20255d190d58529000c06f79c423860a121
Kolinzoviola <dockolinz@gmail.com>
2023-09-25 20:54:22 +0100

    get at index

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index
+ * @head: first node
+ * @index:index return
+ *
+ * Return:nth node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+	listint_t *tmp = head;
+
+	while (tmp && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+
+	return (tmp ? tmp : NULL);
+}
